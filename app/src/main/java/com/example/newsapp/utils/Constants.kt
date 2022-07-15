7dcba364e94bc00256d966ab8b51764c2eb51c6d
Hassan Refaat <76957887+hassanrefaat9@users.noreply.github.com>
2022-07-15 13:28:52 +0200

    init retrofit

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "cca6db3da57c46d28aa8ca8bbe4fb2ca"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
